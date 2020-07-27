4f317e347fde59ca43b75866164286f753dbd1c6
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-27 16:14:23 +1000

    added preventdefault behaviour of form on submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { term: '' };
+  state = {term: ''};
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value}) } />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({term: e.target.value})}
+            />
           </div>
         </form>
       </div>
