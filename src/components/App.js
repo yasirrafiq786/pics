b728dbaa0d17c3746dea5bd10b72cc2595f77553
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-27 17:53:19 +1000

    added unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import axios from 'axios';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
-    }
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID L2L3l2pfv0T1sl4S_0FusntHddU75RB9OZd3JnDr904',
+      },
+    });
+  }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={ this.onSearchSubmit } />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
