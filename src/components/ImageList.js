7e3d96e99c0bbd4bd73d7d59782e0ba1a101264f
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-28 16:30:33 +1000

    made imagecard component work

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css';
 
-const ImageList = props => {
-  const images = props.images.map(({id, urls, description}) => {
-      return <img key={id} src={urls.regular} alt={description} />
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
