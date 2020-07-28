712d8bad5a42f7838fdb88a349f12eb75e338f5a
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-28 17:37:44 +1000

    almost finished the pics app

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,7 +4,7 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { spans: 0 };
+    this.state = {spans: 0};
     this.imageRef = React.createRef();
   }
 
@@ -14,15 +14,15 @@ class ImageCard extends React.Component {
 
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
-    const spans = Math.ceil(height / 150);
-    this.setState({ spans })
+    const spans = Math.ceil(height / 10);
+    this.setState({spans});
   };
 
   render() {
     const {description, urls} = this.props.image;
 
     return (
-      <div style = {{gridRowEnd: `span ${this.state.spans}`}}>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
